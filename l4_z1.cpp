4329714458e6cac34cb49bc9ae8f3c1d14edecba
Gaatayhun <102710478+Gaatayhun@users.noreply.github.com>
2022-04-21 23:02:53 +0800

    Create l4_z1.cpp

---
+++ l4_z1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    int c;
+    cout << "enter the chislo: ";
+    cin >> c;
+    string A[10]={"zero","one","two","thre","four","five","six","seven","eight","nine"};
+    cout << A[c];
+    
+    
+    return 0;
+}
