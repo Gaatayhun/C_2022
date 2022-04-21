e6666038756e534eba9ca6759956e2cae9b076e7
Gaatayhun <102710478+Gaatayhun@users.noreply.github.com>
2022-04-21 16:22:19 +0800

    Create l1_z2.cpp

---
+++ l1_z2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a;
+    int b;
+    short c;
+    long d;
+    cout << "enter  a: " <<endl;
+    cin >> a; 
+    cout << "enter  b: " <<endl;
+    cin >> b; 
+    cout << "enter  c: " <<endl;
+    cin >> c; 
+    cout << "enter  d: " <<endl;
+    cin >> d; 
+    cout <<  a    << " - "<< b << " = " << a-b << endl ;
+    cout <<  b   << " * "<< c << " = " << b*c << endl ;
+    cout <<  c  << " + "<< d << " = " << c+d  << endl ;
+    cout <<  a  << " / "<< d << " = " << a/d << endl ;
+ 
+    return 0;
+}
