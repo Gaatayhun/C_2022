23ae0efcdc1d6b7f54a2db31f2b73cdd46dd7460
Gaatayhun <102710478+Gaatayhun@users.noreply.github.com>
2022-04-25 23:11:10 +0800

    Update l3_z2.cpp

--- l3_z2.cpp
+++ l3_z2.cpp
@@ -4,27 +4,20 @@ using namespace std;
 
 int main()
 {
-    int a, b, res, out,n=0 ;
-    cout << "enter the chislo A: ";
-    cin >> a;
-    cout << "enter the chislo B: ";
-    cin >> b;
-    if (a < b) {
-        while (a <= b) {
-            res = a * 3;
-            a= a + 1;
-            n= n + 1 ;
-            ofstream file("output");
-            file <<  res << '\n';
-            file.close();
-            ifstream fole("output");
-            fole >> out;
-            cout <<n <<', ' << out << '\n';
-            fole.close();
+    int a, b, out,n=1;
+    ifstream file("output");
+    
+    for (;;){
+        file >> out;
+        if (file.fail()){
+            break;
         }
+        cout <<n <<", " << out << '\n';
+        n=n+1;
     }
-    else {
-        cout << "error";
-    }
+    
+    file.close();
+    
+    
     return 0;
 }
